0ec1f9a1892dafe997ccfd6511eb4cc1c89aa8ba
Zloooy <gu1999yra@yandex.ru>
2016-12-01 18:16:28 +0300

    Initial commit

---
+++ app/src/main/java/com/example/student1/whatever/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.student1.whatever;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e("Заголовок", "Hello World");
+                Toast.makeText(MainActivity.this,R.string.message,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
